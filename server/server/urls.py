d897dad88f4b2e8c73a6d2cff27253a4523e5c6c
RizaMamedow <riza.mamedow22@gmail.com>
2023-06-14 16:26:10 +0500

    Update some files, and create README.md

--- server/server/urls.py
+++ server/server/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('api/', include('api.urls'), name='api'),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 else:
     urlpatterns += staticfiles_urlpatterns()
 
